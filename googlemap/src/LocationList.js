5b161085b5dd8928aa61f33205e99d6ffe9c5a3c
@dianavile <diana.vile@gmail.com>
2018-09-13 16:11:09 +0200

    Add LocationList.js file

---
+++ googlemap/src/LocationList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class Locationlist extends React.Component {
+
+    render() {
+        return (
+            <nav>
+                <div className='search-locations'>
+                    <input
+                        type='text'
+                        className=""
+                        placeholder='Search for location'
+                    />
+                </div>
+                <div className="menu">
+                    <ul>
+                        {/*eslint-disable-next-lin */}
+                        <li><a href='#Location1'>BuenasMigas1</a></li>
+                        <li><a href='#Location2'>BuenasMigas2</a></li>
+                        <li><a href='#Location3'>BuenasMigas3</a></li>
+                        <li><a href='#Location4'>BuenasMigas4</a></li>
+                        <li><a href='#Location5'>BuenasMigas5</a></li>
+                        <li><a href='#Location6'>BuenasMigas6</a></li>
+                        <li><a href='#Location7'>BuenasMigas7</a></li>
+                        <li><a href='#Location8'>BuenasMigas8</a></li>
+                        <li><a href='#Location9'>BuenasMigas9</a></li>
+                        <li><a href='#Location10'>BuenasMigas10</a></li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Locationlist
