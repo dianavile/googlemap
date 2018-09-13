8228360fedc06d793eab66e533259f5fb9d53252
@dianavile <diana.vile@gmail.com>
2018-09-13 16:06:08 +0200

    Add: Update app.js

--- googlemap/src/App.js
+++ googlemap/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import LocationList from './LocationList';
+import GoogleMap from './GoogleMap.js';
+import Footer from './Footer.js';
 
 class App extends Component {
-  render() {
-    return (
+/*
+  state = {
+    map: {},
+  }*/
+/*
+  //Initialize google map
+  initMap = () => {
+    const GoogleMap = new window.google.maps.Map(document.getElementById('map'), { //Constructor creates a map centered at Barcelona
+      center: { lat: 41.3851, lng: 2.1734 },  // The location of Barcelona, center and zoom are required.
+      zoom: 12,      
+    });
+*/
+  render(){
+        return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <main>
+          <LocationList />
+          <GoogleMap />  
+            <div id='map'></div>
+        </main>   
+        <Footer />
       </div>
     );
   }
