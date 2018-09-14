1129c7ace8aa6ac75accd4584104319adbf74a04
@dianavile <diana.vile@gmail.com>
2018-09-14 14:07:26 +0200

    Add: Google Map and load function

--- googlemap/src/App.js
+++ googlemap/src/App.js
@@ -2,35 +2,61 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js';
 import LocationList from './LocationList';
-import GoogleMap from './GoogleMap.js';
 import Footer from './Footer.js';
 
 class App extends Component {
-/*
-  state = {
-    map: {},
-  }*/
-/*
-  //Initialize google map
-  initMap = () => {
-    const GoogleMap = new window.google.maps.Map(document.getElementById('map'), { //Constructor creates a map centered at Barcelona
-      center: { lat: 41.3851, lng: 2.1734 },  // The location of Barcelona, center and zoom are required.
-      zoom: 12,      
-    });
+
+
+ /* Show Map on pageload*/
+  componentDidMount() {
+    this.loadGoogleMap()
+  }
+
+/* Add GoogleMaps
+* Load the API from the specified URL- see further loadscript() function
 */
+  loadGoogleMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBTtVemxpZPtEsbI6cw84CSstMfpo90ElU&callback=initMap")
+    window.initMap = this.initMap
+  }
+   
+//Initialize google map
+  // The location of Barcelona, center and zoom are required.
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.3851, lng: 2.1734 },
+    zoom: 12
+  });
+}
+
   render(){
         return (
       <div className="App">
         <Header />
         <main>
-          <LocationList />
-          <GoogleMap />  
-            <div id='map'></div>
+          <div id='map'></div>    
         </main>   
+        <LocationList /> 
         <Footer />
       </div>
     );
   }
+}
+
+/* Add GoogleMaps
+* The async attribute allows the browser to render the page while the API loads
+* The key parameter contain own API key(https://developers.google.com/maps/documentation/javascript/versions)
+* The callback parameter executes the initMap() function
+*/
+
+function loadScript(url){
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+  
 }
 
 export default App;
