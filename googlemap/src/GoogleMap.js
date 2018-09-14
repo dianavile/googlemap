079a1a24177fec5de50383e6dcf077c9bbcb6fcc
@dianavile <diana.vile@gmail.com>
2018-09-14 18:53:02 +0200

    Create GoogleMap.js

---
+++ googlemap/src/GoogleMap.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GoogleMap extends Component {
+    render() {
+        return (
+            <div className="GoogleMap">Add GoogleMap here </div>
+        );
+    }
+}
+
+export default GoogleMap;
